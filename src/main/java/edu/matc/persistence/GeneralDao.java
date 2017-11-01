6818d670ce1ef571fba6714fd71231a1fbb86539
Natasha Grant <ngrant1@madisoncollege.edu>
2017-11-01 13:24:05 -0500

    added more tests

--- src/main/java/edu/matc/persistence/GeneralDao.java
+++ src/main/java/edu/matc/persistence/GeneralDao.java
@@ -1,7 +1,7 @@
 package edu.matc.persistence;
 
 
-import edu.matc.entity.Job;
+import edu.matc.entity.*;
 
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
@@ -11,16 +11,20 @@ import org.hibernate.Transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+
 /**
- * Created by paulawaite on 2/2/16.
+ * Created by ngrant 11/1/17
  */
 public class GeneralDao {
 
     private final Logger log = Logger.getLogger(this.getClass());
 
-    /** Return a list of all categories
+    /** Return a list of all jobs
      *
-     * @return All categories
+     * @return All jobs
      */
     public List<Job> getAllJobs() {
         List<Job> jobs = new ArrayList<Job>();
@@ -29,12 +33,22 @@ public class GeneralDao {
         return jobs;
     }
 
+    /** Return a list of all categories
+     *
+     * @return All categories
+     */
+    public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<Category>();
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        categories = session.createCriteria(Category.class).list();
+        return categories;
+    }
 
     /**
-     * retrieve a user given their id
+     * retrieve a job given their id
      *
-     * @param id the user's id
-     * @return user
+     * @param id the job id
+     * @return job
      */
     public Job getJob(int id) {
         Job job = null;
@@ -43,7 +57,7 @@ public class GeneralDao {
             session = SessionFactoryProvider.getSessionFactory().openSession();
             job = (Job) session.get(Job.class, id);
         } catch (HibernateException hibernateException) {
-            log.error("Error retrieving user with id: " + id, hibernateException);
+            log.error("Error retrieving job with id: " + id, hibernateException);
         } finally {
             if (session != null) {
                 session.close();
@@ -53,26 +67,26 @@ public class GeneralDao {
     }
 
     /**
-     * add a user
+     * add a job
      *
-     * @param user
+     * @param job
      * @return the id of the inserted record
      */
-/**    public int addUser(User user) {
+/**   public int addJob(Job job) {
         int id = 0;
         Transaction transaction = null;
         Session session = null;
         try {
             session = SessionFactoryProvider.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            id = (int) session.save(user);
+            id = (int)session.save(job);
             transaction.commit();
         } catch (HibernateException hibernateException1){
             if (transaction != null) {
                 try {
                     transaction.rollback();
                 } catch (HibernateException hibernateException2) {
-                    log.error("Error rolling back save of user: " + user, hibernateException2);
+                    log.error("Error rolling back save of job: " + job, hibernateException2);
                 }
             }
         } finally {
@@ -82,18 +96,18 @@ public class GeneralDao {
         }
         return id;
     }
+*/
+    public void deleteJob(int id) {
 
-    public void deleteUser(int id) {
-
-        User user = new User();
-        user.setUserid(id);
+        Job job = new Job();
+        job.setJobID(id);
 
         Transaction transaction = null;
         Session session = null;
         try {
             session = SessionFactoryProvider.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.delete(user);
+            session.delete(job);
             transaction.commit();
         } catch (HibernateException hibernateException1){
             if (transaction != null) {
@@ -110,26 +124,26 @@ public class GeneralDao {
         }
 
     }
-*/
+
     /**
-     * Update the user
-     * @param user
+     * Update the job
+     * @param job
      */
 
-/**    public void updateUser(User user) {
+  public void updateUser(Job job) {
         Transaction transaction = null;
         Session session = null;
         try {
             session = SessionFactoryProvider.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.saveOrUpdate(user);
+            session.saveOrUpdate(job);
             transaction.commit();
         } catch (HibernateException hibernateException1){
             if (transaction != null) {
                 try {
                     transaction.rollback();
                 } catch (HibernateException hibernateException2) {
-                    log.error("Error rolling back save of user: " + user, hibernateException2);
+                    log.error("Error rolling back save of user: " + job, hibernateException2);
                 }
             }
         } finally {
@@ -139,6 +153,6 @@ public class GeneralDao {
         }
     }
 
-*/
+
 
 }
--- src/test/java/edu/matc/persistence/GeneralDaoTest.java
+++ src/test/java/edu/matc/persistence/GeneralDaoTest.java
@@ -1,7 +1,7 @@
 package edu.matc.persistence;
 
 
-import edu.matc.entity.Job;
+import edu.matc.entity.*;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.junit.Before;
@@ -17,6 +17,7 @@ public class GeneralDaoTest {
 
     GeneralDao dao;
     int initialJobRecordAmount;
+    int initialCategoryRecordAmount;
 
     @Before
     public void setup() {
@@ -30,13 +31,49 @@ public class GeneralDaoTest {
         assertTrue(jobs.size() > 0);
     }
 
+    @Test
+    public void getAllCategories() throws Exception {
+        List<Category> categories= dao.getAllCategories();
+        assertTrue(categories.size() > 0);
+    }
+
 
     @Test
     public void getJob() throws Exception {
         Job job = dao.getJob(3);
         assertTrue(job.getJobID() == 3);
     }
+/**    @Test
+    public void addJob() throws Exception {
+        Job job = new Job();
+        job.setJobName("IT Help Desk");
+        int jobID = dao.addJob(job);
 
+        assertTrue(job.getJobID() == 4);
+    }
+*/
+
+    @Test
+    public void deleteJob() throws Exception {
+        Job job = dao.getJob(3);
+        dao.deleteJob(3);
+        List<Job> jobs = dao.getAllJobs();
+
+        assertFalse(jobs.contains(jobs));
+    }
+
+    @Test
+    public void updateJob() throws Exception {
+        Job job = dao.getJob(3);
+        String newJobName= "IT Help Desk";
+
+        job.setJobName(newJobName);
+
+        dao.updateUser(job);
+
+        assertTrue(dao.getJob(3).getJobName().equals(newJobName));
+
+    }
 
 
 
