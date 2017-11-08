e78bd425cee050c2037265f13990910e32d1373a
Norskknight <johnspink11i@yahoo.com>
2017-11-07 23:11:34 -0600

    worked on rest to have the uris work

--- src/main/java/edu/matc/rest/RestService.java
+++ src/main/java/edu/matc/rest/RestService.java
@@ -4,18 +4,27 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import edu.matc.persistence.GeneralDao;
+import edu.matc.entity.*;
 
-@Path("/hello")
+import java.util.List;
+
+@Path("/{category}/flashCards")
 
 public class RestService {
+
     // The Java method will process HTTP GET requests
     @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-    public Response getMessage() {
-        // Return a simple message
-        String output = "Hello World";
-        return Response.status(200).entity(output).build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getMessage(@PathParam("category") int category) {
+        // Return Flashcards by class
+        GeneralDao dao = new GeneralDao();
+        Category cata = dao.getCategory(category);
+        List<Flashcard> output = cata.flashcards;
+
+        return Response.status(200).entity(cata).build();
     }
 }
